0a922f0799973abdbc9abb061ab95ead49dcfda3
Alexandre Studlé <alex.studle@gmail.com>
2017-04-25 10:06:11 +0200

    Initialized and playing around

---
+++ README.md
@@ -0,0 +1,5 @@
+#PHP Version Switcher with GO
+
+this package offers to easily switch between your php version on Ubuntu
+
+
---
+++ switch.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"os"
+	"fmt"
+	"log"
+	"os/exec"
+	"flag"
+)
+
+const (
+	headerRegexp = `^([\w-]+):\s*(.+)`
+	authRegexp   = `^(.+):([^\s].+)`
+)
+
+type headerSlice []string
+
+func (h *headerSlice) String() string {
+	return fmt.Sprintf("%s", *h)
+}
+
+func (h *headerSlice) Set(value string) error {
+	*h = append(*h, value)
+	return nil
+}
+
+var (
+	gopath = os.Getenv("GOPATH")
+	headerslice headerSlice
+
+	f = flag.String("folder", "/usr/bin", "")
+)
+
+var usage = `Usage: php-switch [--options] <version>
+
+	-f Folder where php versions are instaleld. By default it will be /usr/bin
+`
+
+func visit(path string, f os.FileInfo, err error) error {
+	fmt.Printf("Visited: %s\n", path)
+	return nil
+}
+
+func main() {
+	out, err := exec.Command("date").Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("The date is %s\n", out)
+
+	flag.Usage = func() {
+		fmt.Fprint(os.Stderr, fmt.Sprintln(usage))
+	}
+
+
+
+
+	flag.Var(&headerslice, "H", "")
+
+	flag.Parse()
+	if flag.NArg() < 1 {
+		fmt.Println("No Arg !")
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	root := *f
+
+	version := flag.Args()[0]
+	fmt.Println(version)
+
+	out, err = exec.Command("locate " + root + "/php").Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Currently installed versions: \r\n %v", out)
+}
