9de6d52613c2ef912aa8f455db94575024fd163b
Alexandre Studlé <alex.studle@gmail.com>
2017-04-25 23:31:23 +0200

    Clean up and displayed Current used version

--- switch.go
+++ switch.go
@@ -3,7 +3,6 @@ package main
 import (
 	"os"
 	"fmt"
-	"log"
 	"os/exec"
 	"flag"
 	"bufio"
@@ -14,76 +13,40 @@ const (
 	authRegexp   = `^(.+):([^\s].+)`
 )
 
-type headerSlice []string
-
-func (h *headerSlice) String() string {
-	return fmt.Sprintf("%s", *h)
-}
-
-func (h *headerSlice) Set(value string) error {
-	*h = append(*h, value)
-	return nil
-}
-
 var (
 	gopath = os.Getenv("GOPATH")
 	headerslice headerSlice
 
 	f = flag.String("folder", "/usr/bin", "")
+	v = flag.String("version", "", "")
+	usage = `Usage: php-switch [--options]
+		-f Folder where php versions are instaleld. By default it will be /usr/bin
+		-v Version you want to switch to. Is Empty by default`
 )
 
-var usage = `Usage: php-switch [--options] <version>
-
-	-f Folder where php versions are instaleld. By default it will be /usr/bin
-`
-
-func visit(path string, f os.FileInfo, err error) error {
-	fmt.Printf("Visited: %s\n", path)
+//region HeadSlice
+type headerSlice []string
+func (h *headerSlice) String() string {
+	return fmt.Sprintf("%s", *h)
+}
+func (h *headerSlice) Set(value string) error {
+	*h = append(*h, value)
 	return nil
 }
+//endregion
 
-func main() {
-	out, err := exec.Command("date").Output()
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("The date is %s\n", out)
-
-	flag.Usage = func() {
-		fmt.Fprint(os.Stderr, fmt.Sprintln(usage))
-	}
-
-
-
-
-	flag.Var(&headerslice, "H", "")
-
-	flag.Parse()
-	if flag.NArg() < 1 {
-		fmt.Println("No Arg !")
-		flag.Usage()
-		os.Exit(1)
-	}
-
-	root := *f
-
-	version := flag.Args()[0]
-	fmt.Println(version)
-
-	cmdName := "locate"
-	cmdArgs := []string{"-r '^" + root + "/php[0-9]'"}
-
-	cmd := exec.Command(cmdName, cmdArgs...)
-	cmdReader, err := cmd.StdoutPipe()
+func handleCmd (name string, args []string, successMessage string) {
+	cmd := exec.Command(name, args...)
+	reader, err := cmd.StdoutPipe()
 
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "Error creating StdoutPipe for Cmd", err)
 		os.Exit(1)
 	}
 
-	scanner := bufio.NewScanner(cmdReader)
+	scanner := bufio.NewScanner(reader)
 	go func() {
-		fmt.Println("Installed versions \r\n %s")
+		fmt.Println(successMessage)
 		for scanner.Scan() {
 			fmt.Println(scanner.Text())
 		}
@@ -100,4 +63,39 @@ func main() {
 		fmt.Fprintln(os.Stderr, "Error waiting for Cmd", err)
 		os.Exit(1)
 	}
+
+	fmt.Println("\r\n")
+}
+
+func main() {
+	flag.Var(&headerslice, "H", "")
+
+	flag.Parse()
+	if flag.NArg() < 1 {
+		fmt.Println("No Arg !")
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	root := *f
+
+	version := flag.Args()[0]
+	fmt.Println(version)
+
+	//region Current PHP version
+	cmdCurrentVersionName := "php"
+	cmdCurrentversionArgs := []string{"php -r \\@phpinfo\\(\\)\\; | grep 'PHP Version' -m 1"}
+
+	handleCmd(cmdCurrentVersionName, cmdCurrentversionArgs, "Currently used version:")
+	//endregion
+
+
+	//region Locate PHP versions
+	cmdLocateName := "locate"
+	cmdLocateArgs := []string{"-r '^" + root + "/php[0-9]'"}
+
+	handleCmd(cmdLocateName, cmdLocateArgs, "Available versions:")
+	//endregion
+
+
 }
